8f8682669624d77dc567549b0ecaf3a56627088a
Tiait1999 <aitzianethinhinane@gmail.com>
2026-06-04 16:09:44 +0200

    tester

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -35,3 +35,27 @@ test ('login v',{tag:'@regression'},async({page})=>{
     // redirection vers l url boutique
     await expect(page).toHaveURL("https://api.efi-academy.com/e-commerce-test-api/client/shop.php")
 })
+test ('login t',{tag:'@integrartion'},async({page})=>{
+    //aller d abord vers l url
+    await page.goto("https://api.efi-academy.com/e-commerce-test-api/auth/login.php")
+    // saisir email
+     await page.locator("#login-email").fill(" client@boutique.qa")
+    // saisir mot de passe 
+     await page.locator("[data-testid='login-password']").fill("Client123!")
+    // clique sur se connecter
+     await page.getByRole("button",{name:"Se connecter"}).click()
+    // redirection vers l url boutique
+    await expect(page).toHaveURL("https://api.efi-academy.com/e-commerce-test-api/client/shop.php")
+})
+test ('login q',{tag:'@e2n'},async({page})=>{
+    //aller d abord vers l url
+    await page.goto("https://api.efi-academy.com/e-commerce-test-api/auth/login.php")
+    // saisir email
+     await page.locator("#login-email").fill(" client@boutique.qa")
+    // saisir mot de passe 
+     await page.locator("[data-testid='login-password']").fill("Client123!")
+    // clique sur se connecter
+     await page.getByRole("button",{name:"Se connecter"}).click()
+    // redirection vers l url boutique
+    await expect(page).toHaveURL("https://api.efi-academy.com/e-commerce-test-api/client/shop.php")
+})
